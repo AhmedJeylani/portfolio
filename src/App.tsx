9cfbb1d066a2fb85e573cc216afc3687e456e87c
Ahmed Jeylani <ahmedjeylani@hotmail.com>
2020-10-22 23:42:32 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<div className="App">
+				<header className="App-header">
+					<p>Hi my name is Ahmed Jeylani. I am a full stack developer</p>
+				</header>
+			</div>
+		</div>
+	);
 }
 
 export default App;
